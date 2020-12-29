1ca1c00a2078e1fb3a11610162ef07d0d51b647f
chengzh <loveandloss@hotmail.com>
2020-12-29 21:41:55 +0800

    404.Sum_of_Left_Leaves.h add bfs solution

--- easy/404.Sum_of_Left_Leaves.h
+++ easy/404.Sum_of_Left_Leaves.h
@@ -14,6 +14,8 @@ Return 24.
 
 */
 
+#include <queue>
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -37,6 +39,36 @@ private:
         }
         return sum;
     }
+    bool isLeafNode(TreeNode* node) {
+        return (!node->left) && (!node->right);
+    }
+};
+
+class Solution2 {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        std::queue<TreeNode*> q;
+        q.push(root);
+        int sum = 0;
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (node->left) {
+                if (isLeafNode(node->left)) {
+                    sum += node->left->val;
+                } else {
+                    q.push(node->left);
+                }
+            }
+            if (node->right && !isLeafNode(node->right)) {
+                q.push(node->right);
+            }
+        }
+        return sum;
+    }
+private:
     bool isLeafNode(TreeNode* node) {
         return (!node->left) && (!node->right);
     }
