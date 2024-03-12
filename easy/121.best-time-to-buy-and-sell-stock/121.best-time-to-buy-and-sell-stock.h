83f89c60ffe9b093c54156298908604cdd9666e0
chengzh <loveandloss@hotmail.com>
2024-03-12 23:55:05 +0800

    format code of 121

--- easy/121.best-time-to-buy-and-sell-stock/121.best-time-to-buy-and-sell-stock.h
+++ easy/121.best-time-to-buy-and-sell-stock/121.best-time-to-buy-and-sell-stock.h
@@ -2,20 +2,20 @@
 
 class Solution {
 public:
-    int maxProfit(std::vector<int>& prices) {
-      int benefit = 0;
-      int slow = 0;
-      int fast = 1;
-      while (fast <= prices.size() - 1) {
-        int tmp = prices[fast] - prices[slow];
-        if (tmp > benefit) {
-          benefit = tmp;
-        }
-        if (tmp < 0) {
-          slow = fast;
-        }
-        fast++;
+  int maxProfit(std::vector<int>& prices) {
+    int benefit = 0;
+    int slow = 0;
+    int fast = 1;
+    while (fast <= prices.size() - 1) {
+      int tmp = prices[fast] - prices[slow];
+      if (tmp > benefit) {
+        benefit = tmp;
       }
-      return benefit;
+      if (tmp < 0) {
+        slow = fast;
+      }
+      fast++;
     }
+    return benefit;
+  }
 };
